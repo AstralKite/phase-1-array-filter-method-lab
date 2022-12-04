812a4680cd18c2d01b1b94f5c215b4496a2ba849
Fausto Ramirez <ramirezmedia@gmail.com>
2022-12-04 18:06:43 -0500

    commit tree bec46b1ea178f5d53449eead21b54feef307d741

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+function findMatching(drivers, arg){
+    let newArray = [];
+    newArray = drivers.filter( function(person){
+        if(person.toUpperCase() === arg.toUpperCase())
+            return person;
+    });
+    return newArray;
+}
+
+
+function fuzzyMatch(drivers, string){
+    let newArray = [];
+    newArray = drivers.filter( function(iterator){ 
+        if(iterator.substring(0,1) === string.substring(0,1)){
+            return iterator;
+        } 
+    });
+    return newArray;
+}
+
+
+function matchName(drivers, string){
+    let newArray = [];
+    newArray = drivers.filter( function(names){
+        if(names.name === string){
+            return names;
+        }
+    });
+    return newArray;
+} 
